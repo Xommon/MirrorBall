9b0fe99a4ca966434d81dd00bba37c8af03dd08e
Micheal Quentin <mquenti3@gmail.com>
2020-01-12 21:14:26 -0600

    Unnamed Update

---
+++ MirrorBall/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MirrorBall/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            endPosition = Input.mousePosition;
+
+            print((startPosition - endPosition).magnitude);
+            direction = (startPosition - endPosition).magnitude;
+        }
+    }
+}
